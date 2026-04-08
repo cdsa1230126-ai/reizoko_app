57662bd06bf66cf9fcca473229f1a70e6146b469
cdsa1230126-ai <cdsa1230126@gn.iwasaki.ac.jp>
2026-04-08 11:41:45 +0900

    11:41

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2427339757"
+    serviceWorkerVersion: "3907365190"
   }
 });
